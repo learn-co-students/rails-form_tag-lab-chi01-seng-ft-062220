06d57675839e9dfae1513862959d8e587a16ca09
foxten <tenaysiafox@gmail.com>
2020-07-18 14:02:01 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,16 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+    @students = Student.all
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
+    @student.save
+    render plain: params[:student]
+  end
 end
